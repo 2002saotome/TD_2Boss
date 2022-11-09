a0314ffede80b60ce11959295952c55d080236df
2002五月女 <94036178+2002saotome@users.noreply.github.com>
2022-11-09 10:42:59 +0900

    プロジェクト ファイルを追加します。

---
+++ Enemy.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "WorldTransform.h"
+#include "Bullet.h"
+#include <random>
+
+class Enemy {
+public:
+	WorldTransform worldTransForm;
+	int isDead;
+	Vector3 enemyTmp;
+	Vector3 YTmp;
+	//右ベクトル
+	Vector3 enemyRight;
+	//左ベクトル
+	Vector3 enemyLeft;
+	//正面ベクトル
+	Vector3 enemyFront;
+	//加速
+	float speed;
+	//time
+	int time;
+
+	int r = 2;
+
+
+public:
+	Enemy();
+	~Enemy();
+	void CalcVec(Vector3 view);
+	void Update(Vector3 obj);
+	void Pop();
+	void Hit();
+	Vector3 GetWorldPosition() { return Affin::GetWorldTrans(worldTransForm.matWorld_); };
+	void OnColision();
+};
